bedf8492373bbcc6ab2a67fcb028e26f58ef69b6
Andrew Carpenter <andrew@conscious2.com>
2018-05-28 15:06:37 +0100

    Init react app + deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+npm run build
+echo -n "ajcarpenter.com" >> ./build/CNAME
+git add build
+git commit -m "Build"
+git push origin master
+git push origin `git subtree split --prefix build master`:gh-pages --force
